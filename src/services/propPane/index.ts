8bbf4d95cfccaf2f679aa78be1128867ba2eb6ab
mikezimm <49648086+mikezimm@users.noreply.github.com>
2019-10-03 17:48:38 -0400

    Tested all works

--- src/services/propPane/index.ts
+++ src/services/propPane/index.ts
@@ -1,17 +1,4 @@
 
-/*
-
-  IntroPage,
-  WebPartSettingsPage,
-  ListMappingPage,
-
-export * from './DropDownControl';
-export * from './LinkControl';
-export * from './SliderControl';
-export * from './TextFieldControl';
-export * from './ToggleControl';
-*/
-
 export * from './IntroPage';
 export * from './WebPartSettingsPage';
 export * from './ListMappingPage1';
